92aa91a43e7619a3624e93904ddd6619deb44d72
krishkeerthiz <krish71197@gmail.com>
2020-12-14 12:13:59 +0530

    First version of application moved into github

---
+++ stockmgmt/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+
+from .models import Stock,StockHistory
+
+class StockCreateForm(forms.ModelForm):
+	class Meta:
+		model = Stock
+		fields = ['category', 'item_name', 'quantity']
+	def clean_category(self):
+		category = self.cleaned_data.get('category')
+		if not category:
+			raise forms.ValidationError('This field is required')
+		return category
+
+
+	def clean_item_name(self):
+		item_name = self.cleaned_data.get('item_name')
+		if not item_name:
+			raise forms.ValidationError('This field is required')
+
+		for instance in Stock.objects.all():
+			if instance.item_name == item_name:
+				raise forms.ValidationError(item_name + ' does exist already!')
+		return item_name
+
+class StockUploadForm(forms.ModelForm):
+	file = forms.FileField()
+	class Meta:
+		model = Stock
+		fields = ['file']
+
+class StockSearchForm(forms.ModelForm):
+   export_to_CSV = forms.BooleanField(required = False)
+   class Meta:
+    model = Stock
+    fields = ['category', 'item_name']
+
+class StockHistorySearchForm(forms.ModelForm):
+   export_to_CSV = forms.BooleanField(required = False)
+   start_date = forms.DateTimeField(required = False)
+   end_date = forms.DateTimeField(required = False)
+   class Meta:
+    model = StockHistory
+    #widgets = {'start_date' : DateInput()}
+    fields = ['category', 'item_name', 'start_date', 'end_date']
+
+class StockUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Stock
+		fields = ['category', 'item_name', 'quantity']
+
+class IssueForm(forms.ModelForm):
+	class Meta:
+		model = Stock
+		fields = ['issue_quantity', 'issue_to']
+
+
+class ReceiveForm(forms.ModelForm):
+	class Meta:
+		model = Stock
+		fields = ['receive_quantity']
+
+class ReorderLevelForm(forms.ModelForm):
+	class Meta:
+		model = Stock
+		fields = ['reorder_level']
