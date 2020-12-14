96662cc0952ba4f5e5064f47a7f0d1ff2d12d48e
krishkeerthiz <krish71197@gmail.com>
2020-12-14 22:39:02 +0530

    Solved undefined red DATABASES

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 from pathlib import Path
 
@@ -132,6 +131,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
