509dd1bc12442b7442ccddc761fe89983fd61f9a
krishkeerthiz <krish71197@gmail.com>
2020-12-17 16:17:58 +0530

    Stock history updated

--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -159,12 +159,24 @@ def issue_items(request, pk):
 	form = IssueForm(request.POST or None, instance=queryset)
 	if form.is_valid():
 		instance = form.save(commit=False)
-		instance.receive_quantity = 0
+		#instance.receive_quantity = 0
 		instance.quantity -= instance.issue_quantity
 		instance.receive_by = form['issue_to'].value()
 		instance.issue_by = str(request.user)
 		messages.success(request, "Issued SUCCESSFULLY. " + str(instance.quantity) + " " + str(instance.item_name) + "s are now left in Store")
 		instance.save()
+
+		issue_history = StockHistory(
+			id = instance.id, 
+			last_updated = instance.last_updated,
+			category_id = instance.category_id,
+			item_name = instance.item_name, 
+			quantity = instance.quantity, 
+			issue_to = instance.issue_to, 
+			issue_by = instance.issue_by, 
+			issue_quantity = instance.issue_quantity, 
+			)
+		issue_history.save()
 
 		return redirect('/stock_detail/'+str(instance.id))
 		# return HttpResponseRedirect(instance.get_absolute_url())
@@ -184,10 +196,21 @@ def receive_items(request, pk):
 	form = ReceiveForm(request.POST or None, instance=queryset)
 	if form.is_valid():
 		instance = form.save(commit=False)
-		instance.issue_quantity = 0
+		#instance.issue_quantity = 0
 		instance.quantity += instance.receive_quantity
 		instance.receive_by = str(request.user)
 		instance.save()
+
+		receive_history = StockHistory(
+			id = instance.id, 
+			last_updated = instance.last_updated,
+			category_id = instance.category_id,
+			item_name = instance.item_name, 
+			quantity = instance.quantity, 
+			receive_quantity = instance.receive_quantity, 
+			receive_by = instance.receive_by
+			)
+		receive_history.save()
 		messages.success(request, "Received SUCCESSFULLY. " + str(instance.quantity) + " " + str(instance.item_name)+"s are now available in Store")
 
 		return redirect('/stock_detail/'+str(instance.id))
